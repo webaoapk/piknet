cb3fb924c021f6925270e93ecb14f01ea9e8bdb6
Ready-MC <readymc.kont@gmail.com>
2021-03-09 21:56:49 +0300

    piknet5

--- README.md
+++ README.md
@@ -1,76 +1,69 @@
-# deskapp
+<a href="https://github.com/BootstrapDash/PurpleAdmin-Free-Admin-Template" target="_blank"><img src="screenshot.jpg"></a>
 
-DeskApp Admin is a free to use Bootstrap 4 admin template. This template uses the default Bootstrap 4 styles along with a variety of powerful jQuery plugins and tools to create a powerful framework for creating admin panels or back-end dashboards.
+<h1>PurpleAdmin-Free-Admin-Template</h1>
+Purple Admin is a free responsive admin template built with Bootstrap 4. The template has colorful, attractive yet simple and elegant design. The template is well crafted, with all the components neatly and carefully designed and arranged within the template.
 
-Theme uses several libraries for charts, calendar, text forms, date range, upload area, form autocomplete, range slider, progress bars, notifications and much more.
+Purple Admin is packed with all the features that fit your needs but not cramped with components you would not even use. It is an excellent fit to build admin panels, e-commerce systems,  project management systems, CMS or CRM.
 
-We would love to see how you use this awesome admin template. You can notify us about your site. Once the list will grown long enough we will write a post similar to this to showcase the best examples.
+Although the template has a design like none other, it is easily customizable to suit your requirements. Purple Admin comes with a clean and well-commented code that makes it easy to work with the template. Thus making it an ideal pick for jump starting your project.
 
-# Quick start
-Several quick start options are available:
-* Clone the repo: git clone https://github.com/dropways/deskapp.git
-* Install with npm: 
-  `npm install deskapp`
 
-## Built With
-* Bootstrap 4
-* Bootstrap Select
-* Bootstrap Tagsinput
-* Bootstrap Touchspin
-* Bootstrap-wysihtml5
-* Moment Js
-* Clipboard Js
-* Air Datepicker
-* Cropperjs
-* Datatables
-* Dropzone
-* Fancybox 3
-* Fullcalendar
-* Highcharts
-* Ion-rangeslider
-* jQuery Ascolorpicker
-* jQuery Asgradient
-* jQuery Knob
-* jQuery Steps
-* Jvectormap
-* Mcustom scrollbar
-* Plyr
-* Select2
-* Slick Slider
-* Sweetalert2
-* Switchery
-* Timedropper
+<h1>Credits:</h1>
 
-## Sample Screens
-<img src="https://user-images.githubusercontent.com/38377336/86491687-03ca9a00-bd89-11ea-8a3a-11dcfcddd254.png" alt="Template 1">
+- Bootstrap 4
+- Font Awesome
+- jQuery
+- Gulp
+- Chart.js
 
-## Demo link
-A fully functional demo is available at
+<h1>Browser Support:</h1>
 
-- demo 1 https://dropways.github.io/deskapp/
-- demo 2 https://deskapp-dashboard.herokuapp.com/
+Purple Admin is designed to work flawlessly with all the latest and modern web browsers.
 
-## Bugs Reports
-Have a bug or a feature request? Please open a new issue.
+- Chrome (latest)
+- FireFox (latest)
+- Safari (latest)
+- Opera (latest)
+- IE10+  
 
-# MIT License
+<h1>License Information:</h1>
 
-Copyright (c) 2018 DeskApp
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+Purple Admin is released under MIT license. Purple Admin is a free Bootstrap 4 admin template developed from BootstrapDash. Feel free to download it, use it, share it, get creative with it.
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+<h1>How to use Purple Admin?</h1>
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+
+1 - Click the Clone or Download button in GitHub and download as a ZIP file or you can enter the command git clone https://github.com/BootstrapDash/PurpleAdmin-Free-Admin-Template.git in you terminal to get a copy of this template.
+
+2 - After the files have been downloaded you will get a folder with all the required files
+
+3 - You can install all the dependencies in the template by running the command npm install. All the required files are in the node modules.
+
+4 - Find the file named index.html, check what all components you need. Open the file in a text editor and you can start editing.
+
+5 - Now that your project has now kick-started, all you need to do now is to code, code, and code to your heart's content.
+
+<h1>How to Contribute?:</h1>
+
+
+We love your contributions and we welcome them wholeheartedly. We believe the more the merrier.
+To contribute make sure you have a Node.js and npm installed. Now run the command gulp --version. If the command returns with the Gulp version number, it means you have Gulp installed. If not you need to run the command npm install --global gulp-cli to install Gulp.
+
+<h2>Next</h2>
+
+After Gulp has been installed, follow the steps below to contribute.
+  <br>
+	1 - Fork and clone the repo of Purple Admin.
+  <br>
+	2 - Run the command npm install to install all the dependencies.
+  <br>
+	3 - Enter the command gulp serve. This will open Purple Admin in your default browser.
+  <br>
+	4 - Make your valuable contribution
+  <br>
+	5 - Submit a pull request.
+  <hr>
+	Do you need a template with more features and functionalities? Get more with our collection of the premium template with more plugins, eye catching animations, UI components, and sample pages all fitting together with a high-quality design.
+Visit
+  <a href="https://www.bootstrapdash.com" target="_blank">https://www.bootstrapdash.com</a> for more admin templates.
--- gulpfile.js
+++ gulpfile.js
@@ -1,233 +1,188 @@
-var autoprefixer, browserSync, concat, config, gulp, imagemin, path, plumber, rename, sass, streamqueue, uglify,changed,reload, cleanCSS;
+'use strict'
 
-gulp = require('gulp');
-sass = require('gulp-sass');
-csso = require('gulp-csso');
-plumber = require('gulp-plumber');
-rename = require('gulp-rename');
-autoprefixer = require('gulp-autoprefixer');
-concat = require('gulp-concat');
-uglify = require('gulp-uglify');
-imagemin = require('gulp-imagemin');
-streamqueue = require('streamqueue');
-browserSync = require('browser-sync').create();
-changed = require('gulp-changed');
-reload = browserSync.reload;
-cleanCSS = require('gulp-clean-css');
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var filter = require('gulp-filter');
+var mainBowerFiles = require('main-bower-files');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var del = require('del');
+var runSequence = require('run-sequence');
+var replace = require('gulp-replace');
+var injectPartials = require('gulp-inject-partials');
+var inject = require('gulp-inject');
+var sourcemaps = require('gulp-sourcemaps');
 
-config = {
-	nodeDir: './node_modules/'
+gulp.paths = {
+    dist: 'dist',
 };
 
-var path = {
-	styles: [
-		'src/styles/theme.css',
-		'src/styles/style.css',
-		'src/styles/media.css'
-	],
-	corestyle: [
-		'src/plugins/bootstrap/bootstrap.min.css',
-		'src/plugins/malihu-custom-scrollbar-plugin-master/jquery.mCustomScrollbar.css',
-		'src/plugins/bootstrap-wysihtml5-master/bootstrap-wysihtml5.css',
-		'src/plugins/air-datepicker/dist/css/datepicker.css',
-		'src/plugins/timedropper/timedropper.css',
-		'src/plugins/highlight.js/src/styles/solarized-dark.css',
-		'src/plugins/select2/dist/css/select2.css',
-		'src/plugins/bootstrap-select/bootstrap-select.min.css'
-	],
-	icon_styles: [
-		'src/fonts/dropways/dropways.css',
-		'src/fonts/font-awesome/css/font-awesome.css',
-		'src/fonts/foundation-icons/foundation-icons.css',
-		'src/fonts/ionicons-master/css/ionicons.css',
-		'src/fonts/themify-icons/themify-icons.css'
-	],
-	scripts: [
-		'src/scripts/setting.js'
-	],
-	core: [
-		'src/scripts/jquery.min.js',
-		'src/plugins/bootstrap/popper.min.js',
-		'src/plugins/bootstrap/bootstrap.min.js',
-		'src/scripts/moment.js',
-		'src/plugins/malihu-custom-scrollbar-plugin-master/jquery.mCustomScrollbar.js',
-		'src/plugins/wysihtml5-master/dist/wysihtml5-0.3.0.js',
-		'src/plugins/bootstrap-wysihtml5-master/bootstrap-wysihtml5.js',
-		'src/plugins/air-datepicker/dist/js/datepicker.js',
-		'src/plugins/air-datepicker/dist/js/i18n/datepicker.en.js',
-		'src/plugins/timedropper/timedropper.js',
-		'src/plugins/highlight.js/src/highlight.pack.js',
-		'src/plugins/select2/dist/js/select2.full.js',
-		'src/plugins/bootstrap-select/bootstrap-select.min.js',
-		'src/scripts/clipboard.min.js',
-	],
-	fonts: [
-		'src/fonts/dropways/*.*',
-		'src/fonts/font-awesome/fonts/*.*',
-		'src/fonts/foundation-icons/*.*',
-		'src/fonts/ionicons-master/fonts/*.*',
-		'src/fonts/themify-icons/fonts/*.*',
-		'src/fonts/**/*.*',
-	],
-	images: 'src/images/**/*.*',
-	php: ['*.html'],
-};
-
-gulp.task('styles', function() {
-	var stream;
-	stream = streamqueue({
-		objectMode: true
-	});
-	stream.queue(gulp.src(path.styles));
-	return stream.done()
-					.pipe(plumber())
-					.pipe(csso())
-					.pipe(autoprefixer({browsers: ['last 2 versions'],cascade: false}))
-					.pipe(concat('style.css'))
-					.pipe(gulp.dest('vendors/styles/'))
-					.pipe(cleanCSS())
-					.pipe(rename({suffix: '.min'}))
-					.pipe(plumber.stop())
-					.pipe(gulp.dest('vendors/styles/'))
-					.pipe(browserSync.reload({stream: true}));
-});
-
-gulp.task('corestyle', function() {
-	var stream;
-	stream = streamqueue({
-		objectMode: true
-	});
-	stream.queue(gulp.src(path.corestyle));
-	return stream.done()
-					.pipe(plumber())
-					.pipe(sass())
-					.pipe(autoprefixer({browsers: ['last 2 versions'],cascade: false}))
-					.pipe(concat('core.css'))
-					.pipe(gulp.dest('vendors/styles/'))
-					.pipe(cleanCSS())
-					.pipe(rename({suffix: '.min'}))
-					.pipe(plumber.stop())
-					.pipe(gulp.dest('vendors/styles/'))
-					.pipe(browserSync.reload({stream: true}));
-});
-
-gulp.task('icon_styles', function() {
-	var stream;
-	stream = streamqueue({
-		objectMode: true
-	});
-	stream.queue(gulp.src(path.icon_styles));
-	return stream.done()
-					.pipe(plumber())
-					.pipe(sass())
-					.pipe(autoprefixer({browsers: ['last 2 versions'],cascade: false}))
-					.pipe(concat('icon-font.css'))
-					.pipe(gulp.dest('vendors/styles/'))
-					.pipe(cleanCSS())
-					.pipe(rename({suffix: '.min'}))
-					.pipe(plumber.stop())
-					.pipe(gulp.dest('vendors/styles/'))
-					.pipe(browserSync.reload({stream: true}));
-});
+var paths = gulp.paths;
 
 
-gulp.task('scripts', function() {
-	var stream;
-	stream = streamqueue({
-		objectMode: true
-	});
-	stream.queue(gulp.src(path.scripts));
-    return stream.done()
-                    .pipe(plumber())
-                    .pipe(concat('script.js'))
-                    .pipe(gulp.dest('vendors/scripts/'))
-                    .pipe(rename({suffix: '.min'}))
-                    .pipe(uglify()).pipe(plumber.stop())
-                    .pipe(gulp.dest('vendors/scripts/'))
-                    .pipe(browserSync.reload({stream: true}));
-});
 
-gulp.task('core', function() {
-	var stream;
-	stream = streamqueue({
-		objectMode: true
-	});
-	stream.queue(gulp.src(path.core));
-    return stream.done()
-                    .pipe(plumber())
-                    .pipe(concat('core.js'))
-                    .pipe(gulp.dest('vendors/scripts/'))
-                    .pipe(rename({suffix: '.min'}))
-                    .pipe(uglify()).pipe(plumber.stop())
-                    .pipe(gulp.dest('vendors/scripts/'))
-                    .pipe(browserSync.reload({stream: true}));
-});
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
 
-gulp.task('php', function() {
-	var stream;
-	stream = streamqueue({
-		objectMode: true
-	});
-	stream.queue(gulp.src(path.php));
-    return stream.done()
-                    .pipe(browserSync.reload({stream: true}));
-});
-
-gulp.task('fonts', function() {
-	var stream;
-	stream = streamqueue({
-		objectMode: true
-	});
-	stream.queue(gulp.src(path.fonts));
-    return stream.done()
-                    .pipe(gulp.dest('vendors/fonts/'));
-});
-
-gulp.task('images', function() {
-	var stream;
-	stream = streamqueue({
-		objectMode: true
-	});
-	stream.queue(gulp.src(path.images));
-	return stream.done().pipe(changed('vendors/images/')).pipe(imagemin({
-		optimizationLevel: 3,
-		progressive: true,
-		interlaced: true,
-		svgoPlugins: [
-			{
-				removeViewBox: false
-			}
-		]
-	})).pipe(gulp.dest('vendors/images/'));
-});
-
-gulp.task('connect-sync', function (done) {
-	browserSync.reload();
-	done();
     browserSync.init({
-      proxy: 'localhost/deskapp', // Change this value to match your local URL.
-	    socket: {
-	      domain: 'localhost:3000'
-	    }
-	});
-	gulp.watch("*.html").on("change", reload);
-	gulp.watch("src/styles/**/*.css").on("change", reload);
-	gulp.watch("src/plugins/**/*.css").on("change", reload);
+        port: 3000,
+        server: "./",
+        ghostMode: false,
+        notify: false
+    });
+
+    gulp.watch('scss/**/*.scss', ['sass']);
+    gulp.watch('**/*.html').on('change', browserSync.reload);
+    gulp.watch('js/**/*.js').on('change', browserSync.reload);
+
 });
 
-gulp.task('watch', function(){
-	gulp.watch("src/styles/**/*.*", gulp.series('styles'));
-	gulp.watch("src/styles/**/*.*", gulp.series('corestyle'));
-	gulp.watch("src/fonts/**/*", gulp.series('fonts'));
-	gulp.watch("src/scripts/**/*.js", gulp.series('scripts'));
-	gulp.watch("src/core/**/*.js", gulp.series('core'));
+
+
+// Static Server without watching scss files
+gulp.task('serve:lite', function() {
+
+    browserSync.init({
+        server: "./",
+        ghostMode: false,
+        notify: false
+    });
+
+    gulp.watch('**/*.css').on('change', browserSync.reload);
+    gulp.watch('**/*.html').on('change', browserSync.reload);
+    gulp.watch('js/**/*.js').on('change', browserSync.reload);
+
 });
 
-gulp.task('default', gulp.series(gulp.parallel(['styles'], 'corestyle', 'fonts', 'scripts', 'core', 'icon_styles', ['connect-sync']), function(){
-	gulp.watch("src/styles/**/*.css", gulp.series('styles'));
-	gulp.watch("src/fonts/**/*", gulp.series('fonts'));
-	gulp.watch("src/styles/**/*.*", gulp.series('corestyle'));
-	gulp.watch("src/scripts/**/*.js", gulp.series('scripts'));
-	gulp.watch("src/scripts/**/*.js", gulp.series('core'));
-	gulp.watch("src/fonts/**/*.*", gulp.series('icon_styles'));
-}));
+
+
+gulp.task('sass', function () {
+    return gulp.src('./scss/style.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream());
+});
+
+
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./scss/**/*.scss');
+});
+
+
+
+gulp.task('clean:dist', function () {
+    return del(paths.dist);
+});
+
+
+
+gulp.task('copy:bower', function () {
+    return gulp.src(mainBowerFiles(['**/*.js', '!**/*.min.js']))
+        .pipe(gulp.dest(paths.dist+'/js/libs'))
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest(paths.dist+'/js/libs'));
+});
+
+
+
+gulp.task('copy:css', function() {
+   gulp.src('./css/**/*')
+   .pipe(gulp.dest(paths.dist+'/css'));
+});
+
+
+
+gulp.task('copy:img', function() {
+   return gulp.src('./img/**/*')
+   .pipe(gulp.dest(paths.dist+'/img'));
+});
+
+
+
+gulp.task('copy:fonts', function() {
+   return gulp.src('./fonts/**/*')
+   .pipe(gulp.dest(paths.dist+'/fonts'));
+});
+
+
+
+gulp.task('copy:js', function() {
+   return gulp.src('./js/**/*')
+   .pipe(gulp.dest(paths.dist+'/js'));
+});
+
+
+
+gulp.task('copy:html', function() {
+   return gulp.src('./**/*.html')
+   .pipe(gulp.dest(paths.dist+'/'));
+});
+
+
+
+gulp.task('replace:bower', function(){
+    return gulp.src([
+        './dist/*.html',
+        './dist/**/*.js',
+    ], {base: './'})
+    .pipe(replace(/bower_components+.+(\/[a-z0-9][^/]*\.[a-z0-9]+(\'|\"))/ig, 'js/libs$1'))
+    .pipe(gulp.dest('./'));
+});
+
+
+
+gulp.task('build:dist', function(callback) {
+    runSequence('clean:dist', 'copy:bower', 'copy:css', 'copy:img', 'copy:fonts', 'copy:js', 'copy:html', 'replace:bower', callback);
+});
+
+
+
+/*sequence for injecting partials and replacing paths*/
+gulp.task('inject', function() {
+  runSequence('injectPartial' , 'injectAssets' , 'replacePath');
+});
+
+
+
+/* inject partials like sidebar and navbar */
+gulp.task('injectPartial', function () {
+  return gulp.src("./**/*.html", { base: "./" })
+    .pipe(injectPartials())
+    .pipe(gulp.dest("."));
+});
+
+
+
+/* inject Js and CCS assets into HTML */
+gulp.task('injectAssets', function () {
+  return gulp.src('./**/*.html')
+    .pipe(inject(gulp.src(['./node_modules/mdi/css/materialdesignicons.min.css', './node_modules/perfect-scrollbar/dist/css/perfect-scrollbar.min.css' , './node_modules/jquery/dist/jquery.min.js', './node_modules/popper.js/dist/umd/popper.min.js' , './node_modules/bootstrap/dist/js/bootstrap.min.js' , './node_modules/perfect-scrollbar/dist/js/perfect-scrollbar.jquery.min.js'], {read: false}), {name: 'plugins', relative: true}))
+    .pipe(inject(gulp.src(['./css/*.css' , './js/off-canvas.js' , './js/misc.js'], {read: false}), {relative: true}))
+    .pipe(gulp.dest('.'));
+});
+
+
+
+/*replace image path and linking after injection*/
+gulp.task('replacePath', function(){
+  gulp.src('pages/*/*.html', { base: "./" })
+    .pipe(replace('src="images/', 'src="../../images/'))
+    .pipe(replace('href="pages/', 'href="../../pages/'))
+    .pipe(replace('href="index.html"', 'href="../../index.html"'))
+    .pipe(gulp.dest('.'));
+  gulp.src('pages/*.html', { base: "./" })
+    .pipe(replace('src="images/', 'src="../images/'))
+    .pipe(replace('"pages/', '"../pages/'))
+    .pipe(replace('href="index.html"', 'href="../index.html"'))
+    .pipe(gulp.dest('.'));
+});
+
+
+
+gulp.task('default', ['serve']);
